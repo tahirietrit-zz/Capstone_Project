b8eaf89282e5a42753310d3ba21a010f1f37db6e
tahirietrit <tahirietrit@gmail.com>
2016-05-04 07:36:05 +0200

    Home Updates

--- app/src/main/java/com/tahirietrit/ireport/MainActivity.java
+++ app/src/main/java/com/tahirietrit/ireport/MainActivity.java
@@ -2,12 +2,23 @@ package com.tahirietrit.ireport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+    @Bind(R.id.fragmentContainer)
+    RelativeLayout fragmentContainer;
+    @Bind(R.id.feed_button)
+    Button feedButton;
+    @Bind(R.id.profile_button)
+    Button profileButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
---
+++ app/src/main/java/fragments/FeedFragment.java
@@ -0,0 +1,21 @@
+package fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tahirietrit.ireport.R;
+
+/**
+ * Created by macb on 12/04/16.
+ */
+public class FeedFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.feed_fragment_layout,container);
+    }
+}
